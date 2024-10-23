51e32c087b3c6945f5dd7b25efcaa4d02722d19f
petar-Atanasov <PA673@live.mdx.ac.uk>
2024-10-23 11:51:07 +0100

    creating the GradeTests

--- src/GradeTest.java
+++ src/GradeTest.java
@@ -1,12 +1,37 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ValueSource;
+import java.util.stream.Stream;
 
 class GradeTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	@ParameterizedTest
+	@ValueSource(ints = { 1, 20 }) // valid grades
+	void testGradeConstructorValid(int grade) {
+		assertDoesNotThrow(() -> new Grade(grade));
 	}
 
+	@ParameterizedTest
+	@ValueSource(ints = { 0, 21 }) // testing invalid grades
+	void testGradeConstructorInvalid(int grade) {
+		Exception exception = assertThrows(IllegalArgumentException.class, () -> new Grade(grade),
+				"Expected from the constructor to throw, but it didn't");
+		assertTrue(exception.getMessage().contains("The grades must be between 0 and 20"));
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = { -1, 0, 100 }) // valid percentage
+	void testFromPercentageValid(int percentage) {
+		assertDoesNotThrow(() -> Grade.fromPercentage(percentage));
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = { -2, 101 }) // testing invalid values for fromPercentage
+	void testFromPercentageInvalid(int percentage) {
+		Exception exception = assertThrows(IllegalArgumentException.class, () -> Grade.fromPercentage(percentage),
+				"Expected from fromPercentage() to throw, but it didn't");
+		assertTrue(exception.getMessage().contains("The percentage must be between 0, 100 or excatly -1"));
+	}
 }
